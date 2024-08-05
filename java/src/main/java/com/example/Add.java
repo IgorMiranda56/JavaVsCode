61d2ea9b57e56858fdcff645b61fbdfa3b3a87f9
IgorMiranda56 <159519511+IgorMiranda56@users.noreply.github.com>
2024-08-05 14:24:29 +0000

    Java

---
+++ java/src/main/java/com/example/Add.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Add 
+{
+    public static void main( String[] args )
+    {
+        //Declaração de variáveis.
+        boolean temSol = true;
+        boolean folga = false;
+        
+        //Só irá passear se as duas condições forem verdadeiras.
+        if (temSol && folga){
+        System.out.println("Irei passear hoje! =D");
+        }else{
+        System.out.println("Não irei passear! =C");
+     }
+    }
+}
